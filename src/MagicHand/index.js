595efa5caffd563a7a12bdc3c4df521c0031ab64
wooknick <wooknick@naver.com>
2022-01-09 23:59:08 +0900

    "feat" basic gesture trigger

--- src/MagicHand/index.js
+++ src/MagicHand/index.js
@@ -12,8 +12,26 @@ class MagicHand {
     document.body.appendChild(this.videoElm);
     this.canvas = document.createElement("canvas");
     this.canvas.id = "debug";
-
     this.ctx = this.canvas.getContext("2d");
+    // LifeCycle
+    this.isDebug = false;
+    // Detection Data
+    this.handIdx = {
+      0: undefined,
+      1: undefined,
+    };
+    this.existHand = {
+      left: false,
+      right: false,
+    };
+    this.handPose = {
+      left: "noexist",
+      right: "noexist",
+    };
+    this.handPosition = {
+      left: [],
+      right: [],
+    };
   }
 
   start() {
@@ -25,6 +43,7 @@ class MagicHand {
   }
 
   debug() {
+    this.isDebug = true;
     document.body.appendChild(this.canvas);
   }
 
@@ -54,7 +73,7 @@ class MagicHand {
     this.camera.start();
   }
 
-  onResults(results) {
+  debugResult(results) {
     this.ctx.save();
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = "#3a3d45";
@@ -73,13 +92,101 @@ class MagicHand {
           lineWidth: 5,
         });
         drawLandmarks(this.ctx, landmarks, { color: "#FF0000", lineWidth: 2 });
-        if (landmarks[0]) {
-          console.log(landmarks[0]["x"], landmarks[0]["y"], landmarks[0]["z"]);
-        }
       }
     }
     this.ctx.restore();
-    // const { multiHandLandmarks: data } = results;
+  }
+
+  onResults(results) {
+    if (this.isDebug) {
+      this.debugResult(results);
+    }
+
+    this.existHand = {
+      left: false,
+      right: false,
+    };
+
+    const { multiHandLandmarks, multiHandWorldLandmarks, multiHandedness } =
+      results;
+
+    // Left or Right
+    for (let i = 0; i < 2; i++) {
+      const hand = multiHandedness[i];
+      if (hand) {
+        this.handIdx[i] = `${hand.label}`.toLowerCase();
+      } else {
+        this.handIdx[i] = undefined;
+      }
+    }
+    for (let i = 0; i < 2; i++) {
+      const hand = this.handIdx[i]; // left or right
+      if (hand) {
+        this.existHand[hand] = true;
+        const landmark = multiHandLandmarks[i];
+        const worldLandmark = multiHandWorldLandmarks[i];
+        if (landmark) {
+          this.handPose[hand] = this.findGesture({ landmark, worldLandmark });
+        } else {
+          this.handPose[hand] = "unknown";
+        }
+      }
+    }
+    // 보이지 않는 손 초기화
+    if (!this.existHand.left) {
+      this.handPose.left = "noexist";
+    }
+    if (!this.existHand.right) {
+      this.handPose.right = "noexist";
+    }
+  }
+
+  findGesture({ landmark, worldLandmark }) {
+    if (isStretchBack(worldLandmark)) {
+      return "stretchBack";
+    } else if (isStand(landmark)) {
+      return "stand";
+    } else {
+      return "unknown";
+    }
+
+    function isStand(landmark) {
+      // 평범하게 손을 들고 있는 상태 트리거
+      let ret = true;
+      if (
+        landmark[7].y < landmark[8].y ||
+        landmark[11].y < landmark[12].y ||
+        landmark[15].y < landmark[16].y ||
+        landmark[19].y < landmark[20].y
+      ) {
+        ret = false;
+      }
+      return ret;
+    }
+
+    function isStretchBack(landmark) {
+      // 농구공을 들듯 손을 당기는 제스쳐
+      let ret = false;
+      // console.log(
+      //   Math.floor(landmark[0].z * 10000) - Math.floor(landmark[9].z * 10000)
+      // );
+      if (
+        Math.floor(landmark[0].z * 10000) - Math.floor(landmark[9].z * 10000) <
+        -100
+      ) {
+        ret = true;
+      }
+
+      return ret;
+    }
+  }
+
+  onGesture(callback) {
+    setInterval(callback.bind(this, this.handPose), 1000 / 24);
+  }
+
+  onPosition(callback) {
+    setInterval(callback.bind(this, this.handPosition), 1000 / 24);
   }
 
   handleEventListener() {
--- src/app.js
+++ src/app.js
@@ -4,4 +4,10 @@ document.body.onload = () => {
   const magicHand = new MagicHand();
   magicHand.start();
   magicHand.debug();
+  // magicHand.onGesture(handleGesture);
+
+  function handleGesture({ left, right }) {
+    console.log(`Left is ${left}`);
+    console.log(`Right is ${right}`);
+  }
 };
